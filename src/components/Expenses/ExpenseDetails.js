eec3df5cff1f489f528ce7d9bb1f546f2c8704f4
arpitanaskar <arpitanaskarphone@gmail.com>
2024-01-24 20:18:41 +0530

    amunt to 100

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
 import "./ExpenseDetails.css";
 
 const ExpenseDetails = (props) => {
+  const [amount, updateAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    updateAmount("100");
+  };
+
   // console.log("expense details props children are " + props.children);
   const title = props.title;
-  const amount = props.amount;
+  // const amount = props.amount;
   const location = props.location;
 
   return (
@@ -11,6 +18,7 @@ const ExpenseDetails = (props) => {
       <h2>{title}</h2>
       <h3 className="expense-item__location">{location}</h3>
       <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>100$</button>
     </div>
   );
 };
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseDetails from "./ExpenseDetails.js";
 import Card from "../UI/Card.js";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate eDate={props.date} />
@@ -11,11 +18,12 @@ const ExpenseItem = (props) => {
       {/* The variable name of date in the ExpenseDate and here when we are passing props, should be the same. "eDate". we can always use date. I used eDate for clearing my concept. */}
 
       <ExpenseDetails
-        title={props.title}
+        title={title}
         amount={props.amount}
         location={props.locationOfExpenditure}
         // { this locationOfExpenditure is getting passed from App.js where the data mainly belongs.}
       />
+      <button onClick={clickHandler}>Change Title</button>
       <button>Delete</button>
     </Card>
   );
