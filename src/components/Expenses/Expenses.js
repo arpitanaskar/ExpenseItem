f81ec24ac17465c871162f2944bb496889e2332c
arpitanaskar <arpitanaskarphone@gmail.com>
2024-01-28 00:19:02 +0530

    With Dropdown Menu

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,14 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import NewExpense from "../NewExpense/NewExpense";
+import Card from "../UI/Card";
 import { useState } from "react";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
   // const expenses = props.items;
   const [expenses, setExpenses] = useState(props.items);
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => [...prevExpenses, expense]);
@@ -24,11 +27,20 @@ const Expenses = (props) => {
     );
   }
 
+  const filterChangeHandler = (selectedYear) => {
+    // console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <div>
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <NewExpense onAddExpense={addExpenseHandler} />
       <div className="expenses"> {Items} </div>
-    </div>
+    </Card>
   );
 };
 
